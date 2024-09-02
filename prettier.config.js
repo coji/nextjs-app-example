dc631cfea7c8d6e17c0d0899033bd98825b08721
coji <coji@techtalk.jp>
2024-09-02 21:00:03 +0900

    chore: Update package dependencies and add prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['twMerge'],
+}
+
+module.exports = config
